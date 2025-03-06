1615c3e27ef91a7b0c8163a7b6b6ed2d7f46c18e
Manu_Love <107737902+mloveral@users.noreply.github.com>
2025-03-06 14:54:00 -0500

    Stats Service implementado

--- src/main/java/uniandes/dse/examen1/services/StatsService.java
+++ src/main/java/uniandes/dse/examen1/services/StatsService.java
@@ -1,9 +1,14 @@
 package uniandes.dse.examen1.services;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import lombok.extern.slf4j.Slf4j;
+import uniandes.dse.examen1.entities.CourseEntity;
+import uniandes.dse.examen1.entities.RecordEntity;
+import uniandes.dse.examen1.entities.StudentEntity;
 import uniandes.dse.examen1.repositories.CourseRepository;
 import uniandes.dse.examen1.repositories.StudentRepository;
 import uniandes.dse.examen1.repositories.RecordRepository;
@@ -22,11 +27,42 @@ public class StatsService {
     RecordRepository inscripcionRepository;
 
     public Double calculateStudentAverage(String login) {
-        // TODO
+        Optional<StudentEntity> estudiante = estudianteRepository.findByLogin(login);
+        if (estudiante.isEmpty())
+        {
+            
+        }
+        StudentEntity student = estudiante.get();
+
+        Double count = 0.0;
+        Double notasAcumuladas = 0.0;
+        for (RecordEntity record: student.getRecords())
+        {
+            count += 1;
+            notasAcumuladas += record.getFinalGrade();
+        }
+
+        return notasAcumuladas/count;
     }
 
     public Double calculateCourseAverage(String courseCode) {
-        r// TODO
+        Optional<CourseEntity> curso = cursoRepository.findByCourseCode(courseCode);
+        if (curso.isEmpty())
+        {
+
+        }
+
+        CourseEntity course = curso.get();
+
+        Double count = 0.0;
+        Double notasAcumuladas = 0.0;
+        for (RecordEntity record: course.getRecords())
+        {
+            count += 1;
+            notasAcumuladas += record.getFinalGrade();
+        }
+
+        return notasAcumuladas/count;
     }
 
 }
